d0ca8ab98040ff9994a88d5a66541b208faf49fe
Eric Augusto <56616279+ericaugusto-git@users.noreply.github.com>
2022-03-15 08:25:59 -0300

    Update helpers.py

--- finance/helpers.py
+++ finance/helpers.py
@@ -40,7 +40,8 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
+#         api_key = os.environ.get("API_KEY")
+        api_key = pk_e6fde3e1ac674aa88ab20f22e9b9618c
         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
         response = requests.get(url)
         response.raise_for_status()
